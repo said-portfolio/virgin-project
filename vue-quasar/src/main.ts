f7302422ac7be354e6f2c34133482eb9a3dd166d
said <s.bourabi@gmail.com>
2025-03-06 11:20:03 +0000

    first commit

---
+++ vue-quasar/src/main.ts
@@ -0,0 +1,21 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
+import App from './App.vue'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font-latin-ext/roboto-font-latin-ext.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(Quasar)
+
+app.mount('#app')
---
+++ vue-quasar/vite.config.ts
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    vueDevTools(),
+    quasar({
+      sassVariables: fileURLToPath(
+        new URL('./src/quasar-variables.sass', import.meta.url)
+      )
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
